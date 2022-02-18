09996e89734f98166467a9c38955cea6b06142c0
Sup3r-Us3r <maydersonmello@gmail.com>
2022-02-17 22:02:42 -0300

    create parse csv file

---
+++ lib/parser.ex
@@ -0,0 +1,40 @@
+defmodule GenReport.Parser do
+  @month_names %{
+    1 => "janeiro",
+    2 => "fevereiro",
+    3 => "março",
+    4 => "abril",
+    5 => "maio",
+    6 => "junho",
+    7 => "julho",
+    8 => "agosto",
+    9 => "setembro",
+    10 => "outubro",
+    11 => "novembro",
+    12 => "dezembro"
+  }
+
+  def parse_file(filename) do
+    filename
+    |> File.stream!()
+    |> Enum.map(&parse_line(&1))
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(0, &String.downcase/1)
+    |> List.update_at(1, &String.to_integer/1)
+    |> List.update_at(2, &String.to_integer/1)
+    |> List.update_at(3, &get_month_name/1)
+    |> List.update_at(4, &String.to_integer/1)
+  end
+
+  defp get_month_name(month_number) do
+    Map.get(
+      @month_names,
+      String.to_integer(month_number)
+    )
+  end
+end
